bde635225fdb1a578bed4b2458d10a8c86e17a88
ArturMingl <adiscov90@gmail.com>
2023-06-10 01:17:26 +0300

    Task2 Done

--- src/Task2/MainTask2.java
+++ src/Task2/MainTask2.java
@@ -3,5 +3,13 @@ package Task2;
 public class MainTask2 {
     public static void main(String[] args){
 
+        Rectangle rect1 = new Rectangle();
+        System.out.println("rect1:");
+        System.out.printf("Площадь - %.2f, Периметр - %.2f%n", rect1.calculateArea(), rect1.calculatePerimeter());
+
+        Rectangle rect2 = new Rectangle(125.12, 14.02);
+        System.out.println("rect2:");
+        System.out.printf("Площадь - %.2f, Периметр - %.2f%n", rect2.calculateArea(), rect2.calculatePerimeter());
+
     }
 }
--- src/Task2/Rectangle.java
+++ src/Task2/Rectangle.java
@@ -1,5 +1,45 @@
 package Task2;
-
+/*Реализуйте класс "Прямоугольник" (Rectangle), который имеет свойства ширина (width) и высота (height).
+Класс должен обладать следующими методами:
+Конструктор без параметров, который создает прямоугольник с шириной и высотой по умолчанию.
+Конструктор, который принимает значения ширины и высоты и создает прямоугольник с заданными значениями.
+Методы доступа (геттеры и сеттеры) для свойств ширины и высоты.
+Метод "вычислить площадь" (calculateArea), который возвращает площадь прямоугольника (ширина * высота).
+Метод "вычислить периметр" (calculatePerimeter), который возвращает периметр прямоугольника (2 * (ширина + высота)).*/
 public class Rectangle {
+    private double wight;
+    private double height;
 
+    public Rectangle(){
+        wight = height = 1.0;
+    }
+
+    public Rectangle(double wight, double height){
+        this.wight = wight;
+        this.height = height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setWight(double wight) {
+        this.wight = wight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWight() {
+        return wight;
+    }
+
+    public double calculateArea(){
+        return wight * height;
+    }
+
+    public double calculatePerimeter(){
+        return 2 * (wight + height);
+    }
 }
