02c1ee7254a24af49f6c80c3f375d63f6944bebd
ArturMingl <adiscov90@gmail.com>
2023-06-10 00:57:53 +0300

    class Rectangle added

--- src/Task1/MainTask1.java
+++ src/Task1/MainTask1.java
@@ -1,3 +1,5 @@
+package Task1;
+
 public class MainTask1 {
     public static void main(String[] args) {
 
---
+++ src/Task2/MainTask2.java
@@ -0,0 +1,7 @@
+package Task2;
+
+public class MainTask2 {
+    public static void main(String[] args){
+
+    }
+}
